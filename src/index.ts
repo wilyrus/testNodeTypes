c67121e36b49a8bba08f715ce19addb198355c7f
Vladislav Smirnov <vs@comindware.com>
2018-11-01 22:15:58 +0300

    Initial commit.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
+//import * as RootView from '../node_modules/corevue/dist/index.html';
 
 const app = express();
 const articles = [{ title: 'Example' }];
 
 app.set('port', process.env.PORT || 3000);
 
-app.get('/articles', (req, res) => {
-  res.send(articles);
+app.get('/', (req, res) => {
+  res.send('RootView');
 });
 
 app.listen(app.get('port'), () => {
